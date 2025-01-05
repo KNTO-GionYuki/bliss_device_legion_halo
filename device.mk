6e49dfb8298abcaf169d5963864632a1b53678ce
GionYuki <ninetail.chocolate@gmail.com>
2025-01-05 21:09:22 +0900

    初期ROMのAPI変更

--- device.mk
+++ device.mk
@@ -3,6 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
 # A/B update
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # first lunch Android API
-PRODUCT_SHIPPING_API_LEVEL := 31
+PRODUCT_SHIPPING_API_LEVEL := 32
