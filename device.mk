09c4eabbbb7c7179f0a1443b7255cc5bd0d6f3df
KNTO-GionYuki <knto.org.master@gmail.com>
2025-01-07 01:12:04 +0900

    missing library config

--- device.mk
+++ device.mk
@@ -14,3 +14,7 @@ PRODUCT_SHIPPING_API_LEVEL := 32
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+
+    PRODUCT_PACKAGES +=		\
+    vendor.qti.hardware.AGMIPC@1.0-impl		\
+    libagm
