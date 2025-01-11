029124a9e397f41e3d3bf8f29d748e698b8b8f65
KNTO-GionYuki <knto.org.master@gmail.com>
2025-01-11 22:44:31 +0900

    fix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -64,7 +64,7 @@ TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/$(STOCK_KERNEL_ZUI_VER)/dtb
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
 BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive	\
-stack_depot_disable=on kasan.stacktrace=off kvm-arm.mode=protected cgroup_disable=pressure cgroup.memory=nokmem console=ttyMSM0,115200n8 loglevel=6 kpti=0 log_buf_len=256K kernel.panic_on_rcu_stall=1 swiotlb=noforce loop.max_part=7 cgroup.memory=nokmem,nosocket pcie_ports=compat msm_rtb.filter=0x237 allow_mismatched_32bit_el0 kasan=off rcupdate.rcu_expedited=1 rcu_nocbs=0-7 ftrace_dump_on_oops pstore.compress=none cpufreq.default_governor=performance can.stats_timer=0 fsa4480_i2c.async_probe=1 slub_debug=- disable_dma32=on video=vfb:640x400,bpp=32,memsize=3072000 bootconfig buildvariant=user  msm_drm.dsi_display0=qcom,mdss_dsi_nova_nt37701a_dsc_cmd: rootwait ro init=/init
+stack_depot_disable=on kasan.stacktrace=off kvm-arm.mode=protected cgroup_disable=pressure cgroup.memory=nokmem console=ttyMSM0,115200n8 loglevel=6 kpti=0 log_buf_len=256K kernel.panic_on_rcu_stall=1 swiotlb=noforce loop.max_part=7 cgroup.memory=nokmem,nosocket pcie_ports=compat msm_rtb.filter=0x237 allow_mismatched_32bit_el0 kasan=off rcupdate.rcu_expedited=1 rcu_nocbs=0-7 ftrace_dump_on_oops pstore.compress=none cpufreq.default_governor=performance can.stats_timer=0 fsa4480_i2c.async_probe=1 slub_debug=- disable_dma32=on video=vfb:640x400,memsize=3072000 bootconfig buildvariant=user  msm_drm.dsi_display0=qcom,mdss_dsi_nova_nt37701a_dsc_cmd: rootwait ro init=/init
 
 
 BOARD_KERNEL_CMDLINE +=	\
@@ -73,7 +73,7 @@ BOARD_KERNEL_CMDLINE +=	\
 	androidboot.usbcontroller=a600000.dwc3		\
 	androidboot.bootdevice = "1d84000.ufshc"	\
 	androidboot.dtbo_idx = "14"			\
-	androidboot.dtb_idx = "1"			\
+	androidboot.dtb_idx = "1"
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image
