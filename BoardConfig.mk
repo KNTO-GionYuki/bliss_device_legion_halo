12220c6155f587a2c171a8fcd7bd6ff9ed3d1c5d
KNTO-GionYuki <knto.org.master@gmail.com>
2025-01-12 20:39:53 +0900

    define kernel sourcepath

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,7 +59,7 @@ BOARD_USES_QCOM_MERGE_DTBS_SCRIPT := true
 #TARGET_NO_KERNEL := false
 ##for use kernel header begin
 TARGET_KERNEL_CONFIG := halo_defconfig
-TARGET_KERNEL_SOURCE := kernel/lenovo/halo
+TARGET_KERNEL_SOURCE := kernel/legion/halo
 ##for use kernel header end
 BOARD_BOOT_HEADER_VERSION := 4
 STOCK_KERNEL_ZUI_VER := 14.0.697
