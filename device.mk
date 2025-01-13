38068dedf5acd926e482e5b68fa63079bc9e0c15
KNTO-GionYuki <knto.org.master@gmail.com>
2025-01-14 01:22:34 +0900

    dtb image copy

--- device.mk
+++ device.mk
@@ -56,6 +56,9 @@ PRODUCT_PACKAGES +=					\
 PRODUCT_PACKAGES +=	\
 	init.recovery.qcom.rc
 
+##for kernel dtb image
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/prebuilt/$(STOCK_KERNEL_ZUI_VER)/dtb:$(PRODUCT_OUT)/dtb.img
 
 ##init 1st vendor_ramdisk
 #PRODUCT_PACKAGES +=			\
